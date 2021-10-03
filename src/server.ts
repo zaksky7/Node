99e916a2ab1e1053a94f211270dc17965525e24d
zaksky7 <90389434+zaksky7@users.noreply.github.com>
2021-10-03 11:12:27 +0300

    Updated

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
-import express from 'express';
+import express from "express";
 
+import { router } from "./routes";
 
 const app = express();
 
+app.use(express.json());
 
-app.listen(3333);
+app.use(router);
+
+app.listen(3333, () => console.log("Server is running! 🚀"));
